90cc23de42e2764b03dbb910284771d21b1af156
jibin <jibinscaria@webnish.com>
2018-10-29 10:10:04 +0530

    new changes.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,8 @@
 class HomeController < ApplicationController
   def index
     @contact = Contact.new
-    @banner=Banner.where(status: true).last
     @authors=Author.where(status: true)
-    @books=Book.where(status: true)
+    @books=Book.where(status: true).includes(:authors)
     @abouts=About.where(status: true).order('time')
     @services=Service.where(status: true).limit(6)
   end
